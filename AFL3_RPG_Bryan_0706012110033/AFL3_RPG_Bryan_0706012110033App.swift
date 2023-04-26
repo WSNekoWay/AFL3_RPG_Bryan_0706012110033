805472afcdaa39e70c612d740290490f8733272a
WSNekoWay <banthony03@student.ciputra.ac.id>
2023-04-26 15:04:51 +0700

    Initial Commit

---
+++ AFL3_RPG_Bryan_0706012110033/AFL3_RPG_Bryan_0706012110033App.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_RPG_Bryan_0706012110033App.swift
+//  AFL3_RPG_Bryan_0706012110033
+//
+//  Created by MacBook Pro on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_RPG_Bryan_0706012110033App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
